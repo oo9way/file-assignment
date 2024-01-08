c11104b270af233a92cd79bcbbc0d37b125f1c25
MuhammadYusuf Rakhimov <coderogabek@gmail.com>
2024-01-08 23:25:20 +0500

    URL comments

--- api/urls.py
+++ api/urls.py
@@ -20,7 +20,9 @@ urlpatterns = [
     path("files/", UserFilesListView.as_view()),
     path("files/<int:pk>/", UserFileDetailUpdateDeleteView.as_view()),
     path("file-upload/", FileUploadView.as_view()),
+    # Crop video
     path("video-crop/", VideoCropView.as_view()),
+    # Check cropping video status
     path(
         "video-status/<int:pk>/", VideoCropProgressView.as_view(), name="video-status"
     ),
